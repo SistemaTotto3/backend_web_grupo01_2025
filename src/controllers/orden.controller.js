ca058b4f43058966df59394401f2da1408e33902
HasselL <hassel3113r@gmail.com>
2025-09-11 11:54:32 -0600

    Se programo controller y routes de la entidad Orden

--- src/controllers/orden.controller.js
+++ src/controllers/orden.controller.js
@@ -1,12 +1,52 @@
 import { pool } from "../../db_connection.js";
 // Obtener todas las categorías
-export const obtenerOrden = async (req, res) => {
+export const obtenerOrdenes = async (req, res) => {
   try {
     const [result] = await pool.query("SELECT * FROM Orden");
     res.json(result);
   } catch (error) {
     return res.status(500).json({
       mensaje: "Ha ocurrido un error al leer los Categorias.",
+      error: error,
+    });
+  }
+};
+
+
+// Obtener una orden por su ID
+export const obtenerOrden = async (req, res) => {
+   try {
+    const idOrden = req.params.idOrden;
+    const [result] = await pool.query("SELECT * FROM Orden WHERE idOrden= ?",[idOrden]
+    );
+    if (result.length <= 0) {
+      return res.status(404).json({
+        mensaje: `Error al leer los datos. ID ${idOrden} no encontrado.`,
+      });
+    }
+    res.json(result[0]);
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: "Ha ocurrido un error al leer los datos de las Ordenes.",
+    });
+  }
+};
+ 
+//Crear una nueva orden
+export const registrarOrden = async (req, res) => {
+  try {
+    const {id_venta, fecha_orden} = req.body;
+    const [result] = await pool.query(
+      "INSERT INTO Orden (id_venta, fecha_orden) VALUES (?, ?)",
+      [id_venta, fecha_orden]);
+    res.json({
+      idOrden: result.insertId,
+    id_venta,
+    fecha_orden
+    });
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: "Ha ocurrido un error al crear la Orden.",
       error: error,
     });
   }
--- src/routes/orden.routes.js
+++ src/routes/orden.routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
-import {obtenerOrden} from '../controllers/orden.controller.js';
+import {obtenerOrdenes, obtenerOrden, registrarOrden} from '../controllers/orden.controller.js';
 
 const router = Router();
 
-// Obtener todas las categorías
-router.get('/Orden', obtenerOrden);
+// Obtener todas las ordenes
+router.get('/Orden', obtenerOrdenes);
+
+//Ruta para obtener una Orden por su ID
+router.get('/Orden/:idOrden', obtenerOrden);
+
+// Ruta para crear una nueva Orden
+router.post('/registrarOrden', registrarOrden);
+
 
 export default router;
