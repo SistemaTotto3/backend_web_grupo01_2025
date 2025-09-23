7879f962c9897d496271884907b8a2b58a2c08b0
Angelly <angygonzalezpineda@gmail.com>
2025-09-23 09:57:04 -0600

    Rutas y controladores BRRR

--- src/controllers/clientes.controller.js
+++ src/controllers/clientes.controller.js
@@ -11,3 +11,42 @@ export const obtenerCliente = async (req, res) => {
     });
   }
 };
+
+// Obtener una cliente por su ID
+export const obtenerClientes = async (req, res) => {
+   try {
+    const idCliente = req.params.idCliente;
+    const [result] = await pool.query("SELECT * FROM Cliente WHERE idCliente= ?",[idCliente]
+    );
+    if (result.length <= 0) {
+      return res.status(404).json({
+        mensaje: `Error al leer los datos. ID ${idCliente} no encontrado.`,
+      });
+    }
+    res.json(result[0]);
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: "Ha ocurrido un error al leer los datos de los clientes.",
+    });
+  }
+};
+
+//Registrar un nuevo cliente
+export const registrarCliente = async (req, res) => {
+  try {
+    const {nombre_1, apellido_1,direccion_cliente,telefono_cliente} = req.body;
+    const [result] = await pool.query(
+      "INSERT INTO Cliente (nombre_1, apellido_1,direccion_cliente,telefono_cliente) VALUES (?, ?,?,?)",
+      [nombre_1, apellido_1,direccion_cliente,telefono_cliente]);
+    res.json({
+      idCliente: result.insertId,
+    nombre_1, apellido_1,direccion_cliente,telefono_cliente
+    });
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: "Ha ocurrido un error al crear la Cliente.",
+      error: error,
+    });
+  }
+};
+
--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
-import {obtenerCliente} from '../controllers/clientes.controller.js';
+import {obtenerCliente, registrarCliente} from '../controllers/clientes.controller.js';
 
 const router = Router();
 
 // Obtener todas los clientes
 router.get('/Clientes', obtenerCliente);
+
+// Obtener un Cliente por su ID
+router.get('/Cliente/:id_cliente', obtenerCliente);
+
+// Registrar un nuevo Cliente
+router.post('/registrarCliente', registrarCliente);
+
 
 export default router;
